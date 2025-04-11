072a88c828635d28cf755543325ba9d89f463830
Arthur-25 <111609469+Arthur-25@users.noreply.github.com>
2025-04-11 18:31:29 -0300

    Add files via upload

---
+++ Projeto_Estrutura_De_Dados/src/main/java/projeto_agenda/classes/compromisso.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projeto_agenda.classes;
+
+/**
+ *
+ * @author arthur
+ */
+public class compromisso {
+    public String nome;
+    public String datahorario;
+    public String descricao;
+    public String local;
+    public int prioridade;
+    public String[] listaenvolvidos;
+}
---
+++ Projeto_Estrutura_De_Dados/src/main/java/projeto_agenda/classes/controle_listas.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projeto_agenda.classes;
+
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author arthur
+ */
+public class controle_listas {
+    //variaveis usadas por contatos
+        public static ArrayList<contatos> cont = new ArrayList<contatos>();
+        //variaveis usadas por compromissos
+        public static ArrayList<compromisso> comp = new ArrayList<compromisso>();
+        
+        
+}
